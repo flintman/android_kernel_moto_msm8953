82bee196616e618e2d3900c43ad4e4b91da9913b
Shiyong Li <a22381@motorola.com>
2016-07-20 15:09:26 -0700

    msm: mdss: use param_lock for race of panel access and off
    Use param_lock to avoid race condition between panel DSI access and panel
    off sequence instead of offlock. Holding offlock in DSI access thread
    would potentially block kick-off thread, ending up with frame drop.
    
    Change-Id: I497295446757bc04543f5c838c804042bae3047c
    Signed-off-by: Shiyong Li <a22381@motorola.com>
    Reviewed-on: http://gerrit.mot.com/878298
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1040,7 +1040,6 @@ static int __maybe_unused mdss_fb_set_param(struct device *dev,
 	}
 
 	mutex_lock(&mfd->param_lock);
-	mutex_lock(&ctl->offlock);
 
 	if (param->value == value)
 		goto unlock;
@@ -1063,7 +1062,6 @@ static int __maybe_unused mdss_fb_set_param(struct device *dev,
 	param->value = value;
 
 unlock:
-	mutex_unlock(&ctl->offlock);
 	mutex_unlock(&mfd->param_lock);
 
 	return ret;
