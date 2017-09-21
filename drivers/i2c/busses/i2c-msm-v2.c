MotorolaMobilityLLC/kernel-msm

i2c-msm-v2: Lower initall priority
I2c-msm-v2 dependent on clock_gcc, but they use same
initcall level, and i2c always load first(because of
Makefile sequence). We may lower i2c's level with
arch_initcall_sync.

Of course, it is not a serious 'issue', but I think
we can make it better.

Change-Id: I1f63b48bbe689c9a6891eb82bb4547c57cd9cfee
Signed-off-by: Yiwen Liu <liuyw2@motorola.com>
Reviewed-on: https://gerrit.mot.com/901662
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver <sltawvr@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Igor Kovalenko <igork@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

drivers/i2c/busses/i2c-msm-v2.c
@@ -3057,7 +3057,7 @@ static int i2c_msm_init(void)
 {
 	return platform_driver_register(&i2c_msm_driver);
 }
-arch_initcall(i2c_msm_init);
+arch_initcall_sync(i2c_msm_init);
 
 static void i2c_msm_exit(void)
 {

flintman/android_kernel_moto_msm8953

drivers/i2c/busses/i2c-msm-v2.c
@@ -3108,7 +3108,8 @@ static int i2c_msm_init(void)
 {
 	return platform_driver_register(&i2c_msm_driver);
 }
-subsys_initcall(i2c_msm_init);
+
+subsys_initcall_sync(i2c_msm_init);
 
 static void i2c_msm_exit(void)
 {